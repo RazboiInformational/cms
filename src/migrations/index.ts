f926823a6ad31c7cee641d705d07aa3ab3326ce7
RazboiInformational <razboiinformational322@gmail.com>
2024-11-26 20:18:25 +0200

    feat: migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241126_181642 from './20241126_181642';
+
+export const migrations = [
+  {
+    up: migration_20241126_181642.up,
+    down: migration_20241126_181642.down,
+    name: '20241126_181642'
+  },
+];
